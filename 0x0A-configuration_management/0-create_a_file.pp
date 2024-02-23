5e10e011a57de8c71f9a3327eed09eaa6bbcd83b
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-02-23 22:47:13 +0000

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Create a file in /tmp
 
-file { 'School':
-  ensure  => 'present',
+file { '/tmp/school':
+  ensure  => 'file',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/School',
 }
