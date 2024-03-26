54aebb0b9df3f06d890ad4eab0896b360c1af04d
root <aaichaou.zakaria@gmail.com>
2024-03-26 18:23:46 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,14 +7,13 @@ from requests import get
 from sys import argv
 
 REST_API = "https://jsonplaceholder.typicode.com"
-
+todos = get(f'{REST_API}/todos/').json()
 
 if __name__ == '__main__':
     if len(argv) > 1:
         if argv[-1].isnumeric():
             emp_id = int(argv[1])
             req = get(f'{REST_API}/users/{emp_id}').json()
-            todos = get(f'{REST_API}/todos/').json()
             emp_name = req.get('name')
             tasks = [x for x in todos if x['userId'] == emp_id]
             completed_tasks = [x for x in tasks if x['completed'] is True]
@@ -27,4 +26,4 @@ if __name__ == '__main__':
             )
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
-                    print(f'\t{task["title"]}')
+                    print('\t'.format(task["title"]))
