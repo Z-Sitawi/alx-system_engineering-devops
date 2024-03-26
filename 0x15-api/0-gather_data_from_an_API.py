e7030c1161336956a13cbce586f1d7a216b099cb
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-26 18:47:38 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,17 +3,18 @@
 This script accepts an integer as a parameter, which is the employee ID.
 Then, returns information about his/her to_do list progress.
 """
-from requests import get
-from sys import argv
+import re
+import requests
+import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
-todos = get(f'{REST_API}/todos/').json()
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        if argv[-1].isnumeric():
-            emp_id = int(argv[1])
-            req = get(f'{REST_API}/users/{emp_id}').json()
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            emp_id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(REST_API, emp_id)).json()
+            todos = requests.get('{}/todos'.format(REST_API)).json()
             emp_name = req.get('name')
             tasks = [x for x in todos if x['userId'] == emp_id]
             completed_tasks = [x for x in tasks if x['completed'] is True]
@@ -26,4 +27,4 @@ if __name__ == '__main__':
             )
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
-                    print(f'\t{task["title"]}')
+                    print('\t {}'.format(task.get('title')))
