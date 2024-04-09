333a6ebfe3af1df7bdd4e35d89f2ffb647f316be
Zakaria Aaichaou <125461010+Z-Sitawi@users.noreply.github.com>
2024-04-09 14:42:24 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-""" this model defines a function that queries the Reddit API
-and returns the number of subscribers (not active users, total subscribers)
-for a given subreddit.
-"""
+'''
+    this module contains the function number_of_subscribers
+'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
+    '''
         queries the Reddit API for a given subreddit.
     :param subreddit: the subreddit to handle.
     :return: The number of subscribers (not active users, total subscribers).
              If an invalid subreddit is given, the function should return 0.
-    """
+    '''
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     # A unique User-Agent header to identify the client
     headers = {'User-Agent': 'sitawi/0.0.14'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-
-        subscribers = data['data']['subscribers']
-
-        return subscribers
-    elif response.status_code == 404:  # Subreddit not found
-        return 0
-    else:
+    try:
+        return response.get('data').get('subscribers')
+    except Exception:
         return 0
