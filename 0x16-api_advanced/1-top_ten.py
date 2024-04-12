957f71173053841d05d9e9bbb3c31b9a5fef1254
Zakaria Aaichaou <125461010+Z-Sitawi@users.noreply.github.com>
2024-04-12 02:01:52 +0000

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+this module contains top_ten function
+"""
+import requests
+
+headers = {"User-Agent": "SitawiZak/1.0"}
+
+
+def top_ten(subreddit):
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
