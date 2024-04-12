717e9d664b488725f23e63d502c1d824db904bad
root <aaichaou.zakaria@gmail.com>
2024-04-12 01:54:25 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-this model contains number_of_subscribers method.
+this module contains number_of_subscribers method
 """
 import requests
 
-headers = {"User-Agent": "SitawiZak/1.0"}
+headers = {"User-Agent": "SitawiZakaria/1.0"}
 
 
 def number_of_subscribers(subreddit):
-    """method doc"""
+    """ eturns the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
