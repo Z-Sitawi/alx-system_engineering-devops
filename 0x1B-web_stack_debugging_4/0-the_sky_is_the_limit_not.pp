4fbb3508ef21e9022681034b312d928cc22e80d1
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-04-17 15:46:48 +0100

    fix nginx max request

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# fix nginx to accept and serve more requests
+
+exec { 'increase nginx max open files limit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
+
